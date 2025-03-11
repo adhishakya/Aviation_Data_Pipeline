fe9520f8ef14efce416f65a1cb0f24311f886f41
adhishakya <shakyaadhish@gmail.com>
2025-03-11 23:02:15 +0545

    exception handling

--- db_operation.py
+++ db_operation.py
@@ -3,28 +3,30 @@ from dotenv import load_dotenv
 import os
 from db_connection import connect_to_db
 
-#TODO: Exception handling
 def insert_aviation_data(values):
+    try:
+        credentials = connect_to_db()
+        
+        conn = mysql.connector.connect(
+            host = credentials['host'],
+            user = credentials['user'],
+            password = credentials['password'],
+            database = credentials['database'],
+        )
+
+        cursor = conn.cursor()
+
+        file = open('insert_query.sql','r')
+        query = file.read()
+        file.close()
+
+        values = list(map(tuple, values))
+        cursor.executemany(query, values)
+        conn.commit()
+        print('Data inserted successfully!')
+
+        cursor.close()
+        conn.close()
     
-    credentials = connect_to_db()
-    
-    conn = mysql.connector.connect(
-        host = credentials['host'],
-        user = credentials['user'],
-        password = credentials['password'],
-        database = credentials['database'],
-    )
-
-    cursor = conn.cursor()
-
-    file = open('insert_query.sql','r')
-    query = file.read()
-    file.close()
-
-    values = list(map(tuple, values))
-    cursor.executemany(query, values)
-    conn.commit()
-    print('Data inserted successfully!')
-
-    cursor.close()
-    conn.close()
+    except:
+        print('Failed to insert data.')
--- get_data.py
+++ get_data.py
@@ -2,30 +2,33 @@ import requests
 from config import getUrl
 from db_operation import insert_aviation_data
 
-#TODO: Exception handling
 def get_aviation_data():
-    limit = 100
-    response = requests.get(getUrl(limit))
-    response_json = response.json()
+    try:
+        limit = 100
+        response = requests.get(getUrl(limit))
+        response_json = response.json()
 
-    values = []
-    for i in range (0,limit):
-        values.append(
-            [
-            response_json['data'][i]['flight_date'],
-            response_json['data'][i]['flight_status'],
-            response_json['data'][i]['departure']['airport'],
-            response_json['data'][i]['departure']['timezone'],
-            response_json['data'][i]['departure']['scheduled'],
-            response_json['data'][i]['departure']['estimated'],
-            response_json['data'][i]['arrival']['airport'],
-            response_json['data'][i]['arrival']['timezone'],
-            response_json['data'][i]['arrival']['scheduled'],
-            response_json['data'][i]['arrival']['estimated'],
-            response_json['data'][i]['airline']['name'],
-            response_json['data'][i]['flight']['number'],
-            response_json['data'][i]['flight']['icao']
-            ]
-        )
+        values = []
+        for i in range (0,limit):
+            values.append(
+                [
+                response_json['data'][i]['flight_date'],
+                response_json['data'][i]['flight_status'],
+                response_json['data'][i]['departure']['airport'],
+                response_json['data'][i]['departure']['timezone'],
+                response_json['data'][i]['departure']['scheduled'],
+                response_json['data'][i]['departure']['estimated'],
+                response_json['data'][i]['arrival']['airport'],
+                response_json['data'][i]['arrival']['timezone'],
+                response_json['data'][i]['arrival']['scheduled'],
+                response_json['data'][i]['arrival']['estimated'],
+                response_json['data'][i]['airline']['name'],
+                response_json['data'][i]['flight']['number'],
+                response_json['data'][i]['flight']['icao']
+                ]
+            )
 
-    insert_aviation_data(values)
+        insert_aviation_data(values)
+    
+    except:
+        print('Failed to fetch data.')
