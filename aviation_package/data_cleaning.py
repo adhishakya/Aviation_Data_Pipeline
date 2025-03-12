008fd4a4133ce69a05a6f0ca5f9a0e89f8919c00
adhishakya <shakyaadhish@gmail.com>
2025-03-12 19:48:51 +0545

    fixed datatype issue while uploading cleaned data to mysql

--- aviation_package/data_cleaning.py
+++ aviation_package/data_cleaning.py
@@ -11,8 +11,8 @@ def insert_cleaned_aviation_data():
     base_path = os.path.dirname(os.path.abspath(__file__))
     sql_file_path = os.path.join(base_path, 'insert_cleaned_data_query.sql')
     try:
-        warnings.filterwarnings("ignore", category=UserWarning)
-        warnings.filterwarnings("ignore", category=FutureWarning)
+        warnings.filterwarnings('ignore', category=UserWarning)
+        warnings.filterwarnings('ignore', category=FutureWarning)
 
         logging.info('Starting to data cleaning process...')
         
@@ -29,30 +29,31 @@ def insert_cleaned_aviation_data():
         
         logging.info('Handling missing values...')
         # handling missing values
-        df["airline_name"].replace("empty", pd.NA, inplace=True)
+        df['airline_name'].replace('empty', pd.NA, inplace=True)
         df['arrival_estimated'].fillna(df['arrival_scheduled'], inplace = True) 
         df['departure_estimated'].fillna(df['departure_scheduled'], inplace = True) 
         df['departure_timezone'].fillna(df['arrival_timezone'],inplace = True)
-        df["departure_airport"].fillna("Unknown Airport", inplace=True)
-        df["arrival_airport"].fillna("Unknown Airport", inplace=True)
-        df['arrival_timezone'].fillna("UTC", inplace=True)
+        df['departure_airport'].fillna('Unknown Airport', inplace=True)
+        df['arrival_airport'].fillna('Unknown Airport', inplace=True)
+        df['arrival_timezone'].fillna('UTC', inplace=True)
         df['airline_name'].fillna('Unknown', inplace = True)
         df['flight_number'].fillna('Unknown', inplace=True)
         df['flight_icao'].fillna('Unknown', inplace=True)
 
         logging.info('Handling duplicate data...')
         # handling duplicate data
+        df = df.astype(str)
         df = df.drop_duplicates(subset = df.columns.difference(['id']), keep = 'first')
         df = df.reset_index(drop = True)
 
         logging.info('Reverting datatypes...')
         # reverting to original data type
         df['id'] = df['id'].astype('int64')
-        df['flight_date'] = pd.to_datetime(df['flight_date'], errors='coerce')
-        df['departure_scheduled'] = pd.to_datetime(df['departure_scheduled'], errors='coerce')
-        df['departure_estimated'] = pd.to_datetime(df['departure_estimated'], errors='coerce')
-        df['arrival_scheduled'] = pd.to_datetime(df['arrival_scheduled'], errors='coerce')
-        df['arrival_estimated'] = pd.to_datetime(df['arrival_estimated'], errors='coerce')
+        df['flight_date'] = pd.to_datetime(df['flight_date'], errors='coerce').dt.date
+        df['departure_scheduled'] = pd.to_datetime(df['departure_scheduled'], errors='coerce').dt.strftime('%Y-%m-%d %H:%M:%S')
+        df['departure_estimated'] = pd.to_datetime(df['departure_estimated'], errors='coerce').dt.strftime('%Y-%m-%d %H:%M:%S')
+        df['arrival_scheduled'] = pd.to_datetime(df['arrival_scheduled'], errors='coerce').dt.strftime('%Y-%m-%d %H:%M:%S')
+        df['arrival_estimated'] = pd.to_datetime(df['arrival_estimated'], errors='coerce').dt.strftime('%Y-%m-%d %H:%M:%S')
         
         logging.info('Truncating table data...')
         truncate_query = 'TRUNCATE TABLE aviation_data_cleaned;'
@@ -73,8 +74,8 @@ def insert_cleaned_aviation_data():
         conn.close()
 
     except mysql.connector.Error as err:
-        logging.error(f"MySQL Error: {err}")
+        logging.error(f'MySQL Error: {err}')
     except Exception as e:
-        logging.error(f"Failed to insert data. Error: {str(e)}")
+        logging.error(f'Failed to insert data. Error: {str(e)}')
 
 insert_cleaned_aviation_data()
